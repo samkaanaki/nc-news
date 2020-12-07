b456f76d1e5813d7c3f7c5b8410813988f99eb1b
Sam Aki <sam.k.aki8@gmail.com>
2020-12-07 15:32:03 +0000

    header restructured, hr added below

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ function App() {
         <img
           src="https://i.imgur.com/ws2kAA0.png"
           alt="logo"
-          id="logo-header"
+          id="header-logo"
         />{' '}
-        <h1>spread-it</h1>
-        <p id="interwebs">the front page of the interwebs</p>
+        <h1>shred-it</h1>
+        <p id="waste-time">waste lots of time, then feel guilty</p>
+        <div className="header-right">
+          <p id="welcome-user">Welcome back samaki</p>
+          {/* <img
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c531.png"
+            alt="user logo"
+          /> */}
+        </div>
       </div>
-      <div className="header-right"></div>
+      <hr />
     </header>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from '@reach/router';
+
+class Home extends React.Component{
+    
+
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default Home
